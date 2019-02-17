9a98858b9a15077ae9eb1765fe2079cacb76b2ae
Dexter Taylor <binarymachineshop@gmail.com>
2019-02-16 16:48:54 -0800

    initial setup

---
+++ install_librdkafka.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USAGE_STRING="Usage: install_librdkafka -p <pkg_manager>"
+
+if [ "$#" -ne 2 ]
+then
+    echo $USAGE_STRING
+    exit 1
+fi
+
+PKG_MGR=$2
+
+sudo $PKG_MGR install -y zlib &&
+$PKG_MGR install -y libssl-dev &&
+$PKG_MGR install -y libsasl2-dev
+
+git clone https://github.com/edenhill/librdkafka.git
+cd librdkafka 
+./configure
+make
+sudo make install
