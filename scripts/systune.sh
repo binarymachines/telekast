799f939ef3f5b16d4cae7edd4e52a8f7f3930a41
Dexter Taylor <binarymachineshop@gmail.com>
2019-02-26 22:10:36 -0800

    tweak system tuning script

--- scripts/systune.sh
+++ scripts/systune.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# system tuning for Kafka brokers
+### system tuning for Kafka brokers. Run as superuser.
+
+# back up existing sysctl.conf
+cp /etc/sysctl.conf /etc/sysctl.conf.bak
 
 # network settings
-
 echo 'net.core.wmem_max=16777216' >> /etc/sysctl.conf
 echo 'net.core.rmem_max=16777216' >> /etc/sysctl.conf
 echo 'net.core.somaxconn = 1000' >> /etc/sysctl.conf
 echo 'net.core.netdev_max_backlog = 5000' >> /etc/sysctl.conf
 echo 'net.ipv4.tcp_wmem = 4096 12582912 16777216' >> /etc/sysctl.conf
 echo 'net.ipv4.tcp_rmem = 4096 12582912 16777216' >> /etc/sysctl.conf
-
 
 # virtual memory
 echo 'vm.swappiness = 1' >> /etc/sysctl.conf
